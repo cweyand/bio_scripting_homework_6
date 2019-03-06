4217fd7f3ae8af49c5948d7fdc90f6d8a4975e08
Courtney Weyand <courtney.weyand@auburn.edu>
2019-03-06 14:42:37 -0600

    uncommenting echo statements

--- part_1/string_finder_WEYAND.sh
+++ part_1/string_finder_WEYAND.sh
@@ -6,9 +6,9 @@
 #searching for the word-tomorrow
 
 
-#read -p "enter word to search for in file $1: " tomorrow
+read -p "enter word to search for in file $1: " tomorrow
 
-#echo "The word you chose was $tomorrow"
+echo "The word you chose was $tomorrow"
 
 line_numbers=`grep -n -o "$2" $1 | sort -n | uniq -c | cut -d : -f 1 | awk '{print "For line " $2 " the searched word was found " $1 "}'`
 echo "Summary for file $1:"
